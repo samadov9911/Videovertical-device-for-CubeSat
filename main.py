aa9994846dc75d7da738c06e9f71a88a24bfac73
Ilnar Khairullin <54238699+razbiralochka@users.noreply.github.com>
2023-04-25 02:23:31 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -5,38 +5,45 @@ import cv2
 from scipy.optimize import minimize
 from scipy.optimize import Bounds
 import math
-	
-img = cv2.imread('images/90.jpg')
+
+#Read image from
+img = cv2.imread('images/45.jpg')
 
 
+#Add filter to image for blur
 blur_image = cv2.GaussianBlur(img,(3, 33), 0)
 
 
+#Conver to gray
 gray = cv2.cvtColor(blur_image, cv2.COLOR_BGR2GRAY)
 
 
+#make white and black pixels
+_, thres = cv2.threshold(gray, 80, 255, cv2.THRESH_BINARY)
 
-
-
-ret, thres = cv2.threshold(gray, 80, 255, cv2.THRESH_BINARY)
+#Highlight edges
 edged = cv2.Canny(thres, 10, 255)
 
 
-
+#just size of image
 height = len(gray[:,1])
 width = len(gray[1,:])
 
 
+#Array of white pixels with coords
 indices = np.where(edged != 0)
-
-
-
 X = [elem for elem in indices[1] ]
 Y = [elem for elem in indices[0] ]
 
+
+
+#make array with center
 if len(X) % 2 == 0:
     X.append(min(X))
     Y.append(min(Y))
+
+
+
 
 LeftX =  min(X) 
 RightX = max(X) 
@@ -68,9 +75,9 @@ def error(args):
         sum=sum+((args[0]-X[i])**2+(args[1]-Y[i])**2-args[2]**2)**2
     return sum
 
-result = minimize(error, np.array([0, 0, 4000]), 
+result = minimize(error, np.array([0, 0, 5000]), 
                   method = 'L-BFGS-B', 
-                  bounds=((-10000,10000),(-10000,10000),(10,1e10)))
+                  bounds=((-10000,10000),(-10000,10000),(-10000,10000)))
 
 
 coef = result.x
